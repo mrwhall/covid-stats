ad7eb04fefd77706f47e937ffb64599f0f9d2402
mrwhall <whlmat002@myuct.ac.za>
2021-08-21 11:06:23 +0200

    charts 2.0

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import pandas as pd
 import requests
 import io
 
+def MA(df):
+    return df.diff().rolling(7).mean().round(0)
+
+def per100000(df,columns):
+    data100000 = pd.DataFrame()
+    for province in columns:
+        data100000[province] = data[province] / int(
+            df_pop["population"][df_pop["province"] == provinces[province]]) * 100000
+    data100000["total"]=df['total']/sum(df_pop["population"])*100000
+
+    return data100000
 
 
 st.set_page_config(
@@ -12,7 +23,7 @@ st.set_page_config(
 
 #get csv
 csv=requests.get('https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_provincial_cumulative_timeline_confirmed.csv').content
-df=pd.read_csv(io.StringIO(csv.decode('utf-8')))
+df_cases=pd.read_csv(io.StringIO(csv.decode('utf-8')))
 
 #deaths
 csv_deaths=requests.get('https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_provincial_cumulative_timeline_deaths.csv').content
@@ -29,27 +40,9 @@ df_pop=pd.read_csv(io.StringIO(csv_pop.decode('utf-8')),names=["province","popul
 
 prov_short=['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW',
        'WC']
-#convert dates
-df.date=pd.to_datetime(df.date,format="%d-%m-%Y")
 
-df.dropna(inplace=True)
-data=df[['date','EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW',
-       'WC','total']]
-data[['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW',
-       'WC','total']]=data[['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW',
-       'WC','total']].diff().rolling(7).mean().round(0)
-data.set_index("date",inplace=True)
-data.dropna(inplace=True)
-
-provinces=dict(zip(sorted(prov_short),sorted(list(df_pop["province"]))))
-
-
-data100000=pd.DataFrame()
-for province in prov_short:
-    data100000[province]=data[province]/int(df_pop["population"][df_pop["province"]==provinces[province]])*100000
-
-data100000["total"]=data['total']/sum(df_pop["population"])*100000
-
+provandTotal=['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW',
+       'WC','total']
 
 st.title("Daily New Covid Cases by Province")
 
@@ -58,14 +51,86 @@ all = st.checkbox("Select all")
 
 if all:
     prov = container.multiselect("Choose a province:",
-                                             sorted(list(data.columns)), sorted(list(data.columns)))
+                                             sorted(provandTotal), sorted(provandTotal))
 else:
     prov = container.multiselect("Choose a province:",
-                                             sorted(list(data.columns)),default=['total'])
+                                             sorted(provandTotal),default=['total'])
+
+peaks = df_cases.loc['2020-12-01T00:00:00.000000000':'2021-02-25T00:00:00.000000000'].max()
+
+if prov == []:
+    st.stop()
+
+with st.sidebar:
+    st.title("Overall numbers and daily changes")
+    if "total" in prov:
+        st.metric(label="Total cases",value=f"{df_cases.total.iloc[-1]:,}", delta=f"{df_cases.total.pct_change().iloc[-1]:.1%}")
+
+        st.metric(label="Total deaths",value=f"{df_deaths.total.iloc[-1]:,}", delta=f"{df_deaths.total.pct_change().iloc[-1]:.1%}")
+
+        st.metric(label="Total vaccined",value=f"{df_vacc.total.iloc[-1]:,}", delta=f"{df_vacc.total.pct_change().iloc[-1]:.1%}")
+
+        if 0.3*peaks.total.sum()<df_cases.total.iloc[-1]:
+            st.markdown("_SA is still in the third wave_")
+        else:
+            st.markdown("_SA is no longer in the third wave_")
+    else:
+        st.metric(label="Total cases", value=f"{df_cases[prov].iloc[-1].sum():,}",
+                  delta=f"{df_cases[prov].pct_change().iloc[-1].mean():.1%}")
+
+        st.metric(label="Total deaths", value=f"{df_deaths[prov].iloc[-1].sum():,}",
+                  delta=f"{df_deaths[prov].pct_change().iloc[-1].mean():.1%}")
+
+        st.metric(label="Total vaccined", value=f"{df_vacc[prov].iloc[-1].sum():,}",
+                  delta=f"{df_vacc[prov].pct_change().iloc[-1].mean():.1%}")
+
+        st.subheader("Total cases:")
+        st.write(f"{df_cases[prov].iloc[-1].sum():,}")
+
+        st.subheader("Total deaths:")
+        st.write(f"{df_deaths[prov].iloc[-1].sum():,}")
+
+        st.subheader("Total vaccinated:")
+        st.write(f"{df_vacc[prov].iloc[-1].sum():,}")
+
+        if 0.3*(peaks[prov].sum())<df_cases[prov].iloc[-1].sum():
+            st.markdown("_These/this province(s) are still in the third wave_")
+        else:
+            st.markdown("_These/this province(s) are no longer in the third wave_")
+
+    graphType = st.radio("Toggle graph", ["Daily Cases", "Daily Deaths", "Daily Vaccinated"])
+
+if graphType == "Daily Cases":
+    df = df_cases
+    title="New cases (7 day moving-average)"
+
+elif graphType == "Daily Deaths":
+    df = df_deaths
+    title = "Daily deaths (7 day moving-average)"
+
+elif graphType == "Daily Vaccinated":
+    df=df_vacc
+    title = "Daily vaccinated (7 day moving-average)"
+    df.date = pd.to_datetime(df.date, format="%Y-%m-%d")
+
+#convert dates
+if graphType != "Daily Vaccinated":
+    df.date=pd.to_datetime(df.date,format="%d-%m-%Y")
+
+df.dropna(inplace=True)
+data=df[['date','EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW',
+       'WC','total']]
+data[['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW',
+       'WC','total']]=MA(data[['EC', 'FS', 'GP', 'KZN', 'LP', 'MP', 'NC', 'NW',
+       'WC','total']])
+data.set_index("date",inplace=True)
+data.dropna(inplace=True)
+
+provinces=dict(zip(sorted(prov_short),sorted(list(df_pop["province"]))))
 
 
-#Layout
-col1, col2 = st.beta_columns([10,30])
+data100000=per100000(data,prov_short)
+
 
 st.markdown(
         f"""
@@ -81,46 +146,15 @@ st.markdown(
         unsafe_allow_html=True,
     )
 
-peaks = data.loc['2020-12-01T00:00:00.000000000':'2021-02-25T00:00:00.000000000'].max()
 
-with col1:
-    if "total" in prov:
-        st.subheader("Total cases:")
-        st.write(f"{df.total.iloc[-1].sum():,}")
 
-        st.subheader("Total deaths:")
-        st.write(f"{df_deaths.total.iloc[-1].sum():,}")
-
-        st.subheader("Total vaccinated:")
-        st.write(f"{df_vacc.total.iloc[-1].sum():,}")
-
-        if 0.3*peaks.total.sum()<data.total.iloc[-1]:
-            st.markdown("_SA is still in the third wave_")
-        else:
-            st.markdown("_SA is no longer in the third wave_")
-    else:
-        st.subheader("Total cases:")
-        st.write(f"{df[prov].iloc[-1].sum():,}")
-
-        st.subheader("Total deaths:")
-        st.write(f"{df_deaths[prov].iloc[-1].sum():,}")
-
-        st.subheader("Total vaccinated:")
-        st.write(f"{df_vacc[prov].iloc[-1].sum():,}")
-
-        if 0.3*(peaks[prov].sum())<data[prov].iloc[-1].sum():
-            st.markdown("_These/this province(s) are still in the third wave_")
-        else:
-            st.markdown("_These/this province(s) are no longer in the third wave_")
-
-with col2:
-    if st.checkbox("Per 100,000 people"):
-        st.subheader("New cases per 100,000 people (7 day moving-average)")
-        st.line_chart(data100000[prov], width=850, height=400, use_container_width=False)
-        st.caption(
-            "Data source: [https://github.com/dsfsi/covid19za/tree/master/data](https://github.com/dsfsi/covid19za/tree/master/data)")
-    else:
-        st.subheader("New cases (7 day moving-average)")
-        st.line_chart(data[prov],width=850,height=400,use_container_width=False)
-        st.caption("Data source: [https://github.com/dsfsi/covid19za/tree/master/data](https://github.com/dsfsi/covid19za/tree/master/data)")
+if st.checkbox("Per 100,000 people"):
+    st.subheader(title[:-1]+" per 100,000 people)")
+    st.line_chart(data100000[prov], width=850, height=400, use_container_width=False)
+    st.caption(
+        "Data source: [https://github.com/dsfsi/covid19za/tree/master/data](https://github.com/dsfsi/covid19za/tree/master/data)")
+else:
+    st.subheader(title)
+    st.line_chart(data[prov],width=850,height=400,use_container_width=False)
+    st.caption("Data source: [https://github.com/dsfsi/covid19za/tree/master/data](https://github.com/dsfsi/covid19za/tree/master/data)")
 
